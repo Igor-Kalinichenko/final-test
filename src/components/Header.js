c48a8c4bb1456d7624502a0626978e0d8b8e5e1c
Igor Kalinichenko <kaliv0601@gmail.com>
2022-10-22 18:28:11 +0300

    fixed header menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,17 +20,25 @@ function Header () {
     
     const menuClick = () => {
         if (window.screen.width < 1100) {
-            setShowMenu(showMenu ? false : true);
+            if (!showMenu) {
+                document.getElementById('menu').classList.remove('hideMenu');
+                document.getElementById('menu').classList.add('showMenu');
+                setShowMenu(true);
+            } else {
+                document.getElementById('menu').classList.remove('showMenu');
+                document.getElementById('menu').classList.add('hideMenu');
+                setShowMenu(false);
+            }
         }
     }
     return <>
         <Navbar bg="dark" variant="dark">
-            <Container className='position-relative'>
+            <Container className='position-relative flex-wrap custom-container'>
                 <div className='d-flex'>
                     <div className='menu-icon d-none'><MenuIcon onClick={menuClick}/></div>
                     <Navbar.Brand className='header__logo'><Link to="/"><Logo /></Link></Navbar.Brand>
                 </div>
-                <Nav className={`${showMenu ? 'showMenu' : 'hideMenu'} mr-auto header__nav menu-links`} onClick={menuClick} >
+                <Nav className={`mr-auto header__nav menu-links`} id='menu' onClick={menuClick} >
                     <Link className={'nav-link'} onClick={() => setPoint('Чоловікам')} to="products">Чоловікам</Link>
                     <Link className={'nav-link'} onClick={() => setPoint('Жінкам')} to="products">Жінкам</Link>
                     <Link className={'nav-link'} onClick={() => setPoint('Дітям')} to="products">Дітям</Link>
@@ -38,32 +46,37 @@ function Header () {
                     <Link className={'nav-link text-success'} onClick={() => setNovelty(true)} to="products/new">Новинки</Link>
                     {role==='admin' ? <Link className={'nav-link text-danger'} to="admin">Admin</Link> : ''}
                 </Nav>
-                    <div className='d-flex'>
-                        {isAuth ? <Link className={'nav-link px-3 custom-icons'} to="profile">
-                            <OverlayTrigger placement='bottom' overlay={<Tooltip>Welcome, {firstName}</Tooltip>}>
-                                <Auth />
-                            </OverlayTrigger></Link> : ''}
-                        {isAuth ?
-                            <Link className={'nav-link px-3 custom-icons'} to="logout">
-                                <OverlayTrigger placement='bottom' overlay={<Tooltip>LogOut</Tooltip>}>
-                                    <LogOut />
-                                </OverlayTrigger></Link> : 
-                            <Link className={'nav-link px-3 custom-icons'} to="login">
-                                <OverlayTrigger placement='bottom' overlay={<Tooltip>LogIn</Tooltip>}>
-                                    <LogIn />
-                                </OverlayTrigger></Link> }
+                <div className='d-flex'>
+                    {isAuth ? <>
+                    <Link className={'nav-link px-3 custom-icons'} to="profile">
+                        <OverlayTrigger placement='bottom' overlay={<Tooltip>Welcome, {firstName}</Tooltip>}>
+                            <Auth />
+                        </OverlayTrigger>
+                    </Link>
+                    <Link className={'nav-link px-3 custom-icons'} to="logout">
+                        <OverlayTrigger placement='bottom' overlay={<Tooltip>LogOut</Tooltip>}>
+                            <LogOut />
+                        </OverlayTrigger>
+                    </Link>
+                    </> : 
+                    <Link className={'nav-link px-3 custom-icons'} to="login">
+                        <OverlayTrigger placement='bottom' overlay={<Tooltip>LogIn</Tooltip>}>
+                            <LogIn />
+                        </OverlayTrigger>
+                    </Link>
+                    }
                         
-                        <Link className={'nav-link px-3 custom-icons'} to="contacts">
-                            <OverlayTrigger placement='bottom' overlay={<Tooltip>Контакти</Tooltip>}>
-                                <ContactsIcon />
-                            </OverlayTrigger>
-                        </Link>
-                        <Link className={'nav-link px-3 custom-icons position-relative'} to="cart">
-                            <CartIcon style={{fill: orders.length ? '#198754' : '#fff'}} />
-                            {orders?.length>0 ? 
+                    <Link className={'nav-link px-3 custom-icons'} to="contacts">
+                        <OverlayTrigger placement='bottom' overlay={<Tooltip>Контакти</Tooltip>}>
+                            <ContactsIcon />
+                        </OverlayTrigger>
+                    </Link>
+                    <Link className={'nav-link px-3 custom-icons position-relative'} to="cart">
+                        <CartIcon style={{fill: orders.length ? '#198754' : '#fff'}} />
+                        {orders?.length>0 ? 
                             <Badge pill className='position-absolute' style={{top: '1.2rem', left: '.4rem'}} bg="success">{orders.length}</Badge> : ''}
-                        </Link>
-                    </div>
+                    </Link>
+                </div>
             </Container>
         </Navbar>
     </>
